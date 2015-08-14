8975d8beefd013425b850e70694050d36cb27d3e
Ryota Kimura <ltd.lp.std.car.717@gmail.com>
2015-08-15 00:18:45 +0900

    Initial Commit

---
+++ TrainingLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingLog
+//
+//  Created by Ryota Kimura on 2015/08/15.
+//  Copyright (c) 2015年 kimu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
